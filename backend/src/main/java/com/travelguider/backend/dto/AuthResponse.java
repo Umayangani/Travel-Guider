6149f890d8e5b10dc1f07fea2c6b6f9e0bbc11ee
Umayangani <rmuumayangani@.com>
2025-07-04 20:41:35 +0530

    Auth

--- backend/src/main/java/com/travelguider/backend/dto/AuthResponse.java
+++ backend/src/main/java/com/travelguider/backend/dto/AuthResponse.java
@@ -1,19 +1,5 @@
 package com.travelguider.backend.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Setter
-@Getter
 public class AuthResponse {
-    private String token;
-
-    public AuthResponse() {
-        // default constructor for serialization
-    }
-
-    public AuthResponse(String token) {
-        this.token = token;
-    }
-
+    public String token;
 }
